f72210b651bd41461d02653e5cb8505270ce940d
Mahnaz <mahnazameer123@gmail.com>
2021-06-22 17:52:40 +0600

    Sale post

--- app/src/main/java/csedu/homeclick/androidhomeclick/structure/SaleAdvertisement.java
+++ app/src/main/java/csedu/homeclick/androidhomeclick/structure/SaleAdvertisement.java
@@ -9,7 +9,7 @@ public class SaleAdvertisement extends Advertisement implements Serializable {
 
     private int latitude, longitude;
 
-    public SaleAdvertisement() {
+    public SaleAdvertisement(String areaName, String fullAddress, String sale, int numOfBedrooms, int numOfBathrooms, int numOfBalconies, int floor, int floorSpace, Boolean gasAvail, Boolean elevatorAvail, Boolean generatorAvail, Boolean garageAvail, Boolean securityAvail, int payAmount, String saleDesc, String situation) {
     }
 
     public SaleAdvertisement(String areaName, String fullAddress, String adType, int numberOfBedrooms, int numberOfBathrooms, Boolean gasAvailability, int paymentAmount, User advertiser, int numberOfBalconies, int floor, int floorSpace, Boolean elevator, Boolean generator, Boolean garageSpace, int numberOfImages, String propertyCondition, String description) {
@@ -18,7 +18,7 @@ public class SaleAdvertisement extends Advertisement implements Serializable {
         this.description = description;
     }
 
-    public SaleAdvertisement(String areaName, String fullAddress, String adType, int numberOfBedrooms, int numberOfBathrooms, Boolean gasAvailability, int paymentAmount, int numberOfBalconies, int floor, int floorSpace, Boolean elevator, Boolean generator, Boolean garageSpace, int numberOfImages, String propertyCondition, String description) {
+    public SaleAdvertisement(String name, String address, String sale, String areaName, String fullAddress, String adType, int numberOfBedrooms, int numberOfBathrooms, Boolean gasAvailability, int paymentAmount, int numberOfBalconies, int floor, int floorSpace, Boolean elevator, Boolean generator, Boolean garageSpace, int numberOfImages, String propertyCondition, String description, Boolean securityAvail) {
         super(areaName, fullAddress, adType, numberOfBedrooms, numberOfBathrooms, gasAvailability, paymentAmount, numberOfBalconies, floor, floorSpace, elevator, generator, garageSpace, numberOfImages);
         this.propertyCondition = propertyCondition;
         this.description = description;
