aa775454355129dcb2557d04993ee26f2aab054e
Mahnaz <mahnazameer123@gmail.com>
2021-06-27 13:07:11 +0600

    Filter updated

--- app/src/main/java/csedu/homeclick/androidhomeclick/activities/Filter.java
+++ app/src/main/java/csedu/homeclick/androidhomeclick/activities/Filter.java
@@ -1,9 +1,5 @@
 package csedu.homeclick.androidhomeclick.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,17 +10,22 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import csedu.homeclick.androidhomeclick.R;
 import csedu.homeclick.androidhomeclick.navigator.BottomNavBarHandler;
 import csedu.homeclick.androidhomeclick.navigator.TopAppBarHandler;
 import csedu.homeclick.androidhomeclick.structure.FilterCriteria;
 
-public class Filter extends AppCompatActivity implements View.OnClickListener{
+public class Filter extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "Filter";
     private Toolbar toolbar;
 
-    private EditText paymentAmount, bedroom, bathroom;
+    private TextInputEditText paymentAmount;
+    private EditText filter_no_of_bedrooms, filter_no_of_bathrooms;
     private CheckBox gasAvailability;
     private RadioGroup adType;
     private RadioButton rent, sale, any;
@@ -33,25 +34,48 @@ public class Filter extends AppCompatActivity implements View.OnClickListener{
     private FilterCriteria filterCriteria;
 
 
+    private Button increase_bedrooms, decrease_bedrooms;
+    int count_bedrooms;
+
+    private Button increase_bathrooms, decrease_bathrooms;
+    int count_bathrooms;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
 
         bindWidgets();
+        setClickOnListeners();
+    }
+
+    private void setClickOnListeners() {
+        Log.i(TAG, "in set click listeners");
         applyFilter.setOnClickListener(this);
+        increase_bedrooms.setOnClickListener(this);
+        decrease_bedrooms.setOnClickListener(this);
+        increase_bathrooms.setOnClickListener(this);
+        decrease_bathrooms.setOnClickListener(this);
     }
 
     private void bindWidgets() {
         paymentAmount = findViewById(R.id.filter_max_pay_amount);
-        bedroom = findViewById(R.id.filter_num_of_bedrooms);
-        bathroom = findViewById(R.id.filter_num_of_bathrooms);
         gasAvailability = findViewById(R.id.filter_gas_availability);
         adType = findViewById(R.id.choose_ad_type_radio_group);
         rent = findViewById(R.id.choose_rent);
         sale = findViewById(R.id.choose_sale);
         any = findViewById(R.id.choose_any);
         applyFilter = findViewById(R.id.apply_filter);
+
+        increase_bedrooms = findViewById(R.id.increase_bedrooms);
+        decrease_bedrooms = findViewById(R.id.decrease_bedrooms);
+        increase_bathrooms = findViewById(R.id.increase_bathrooms);
+        decrease_bathrooms = findViewById(R.id.decrease_bathrooms);
+
+
+        filter_no_of_bedrooms = findViewById(R.id.filter_num_of_bedrooms);
+        filter_no_of_bathrooms = findViewById(R.id.filter_num_of_bathrooms);
+
 
         filterCriteria = new FilterCriteria();
 
@@ -60,18 +84,41 @@ public class Filter extends AppCompatActivity implements View.OnClickListener{
 
         toolbar = findViewById(R.id.app_toolbaar);
         TopAppBarHandler.getInstance(toolbar, this).handle();
+
     }
 
     @Override
     public void onClick(View v) {
-        if(noneSelected()) {
-            Log.i(TAG, "none selected");
-            startActivity(new Intent(getApplicationContext(), AdFeed.class));
-        } else {
-            createFilterCriteria();
-            Intent targetIntent = new Intent(getApplicationContext(), AdFeed.class);
-            targetIntent.putExtra("FilterCriteria", this.filterCriteria);
-            startActivity(targetIntent);
+        switch (v.getId()) {
+            case R.id.apply_filter:
+                if (noneSelected()) {
+                    Log.i(TAG, "none selected");
+                    startActivity(new Intent(getApplicationContext(), AdFeed.class));
+                } else {
+                    createFilterCriteria();
+                    Intent targetIntent = new Intent(getApplicationContext(), AdFeed.class);
+                    targetIntent.putExtra("FilterCriteria", this.filterCriteria);
+                    startActivity(targetIntent);
+                }
+                break;
+            case R.id.increase_bedrooms:
+                count_bedrooms++;
+                filter_no_of_bedrooms.setText(Integer.toString(count_bedrooms));
+                break;
+            case R.id.decrease_bedrooms:
+                count_bedrooms--;
+                filter_no_of_bedrooms.setText(Integer.toString(count_bedrooms));
+                break;
+            case R.id.increase_bathrooms:
+                count_bathrooms++;
+                filter_no_of_bathrooms.setText(Integer.toString(count_bathrooms));
+                break;
+            case R.id.decrease_bathrooms:
+                count_bathrooms--;
+                filter_no_of_bathrooms.setText(Integer.toString(count_bathrooms));
+                break;
+            default:
+                break;
         }
     }
 
@@ -85,45 +132,43 @@ public class Filter extends AppCompatActivity implements View.OnClickListener{
             case R.id.choose_sale:
                 filterCriteria.setAdType("Sale");
                 break;
-            default:
-                break;
         }
 
-        if(!paymentAmount.getText().toString().isEmpty()) {
-            int amount = Integer.parseInt( paymentAmount.getText().toString() );
+        if (!paymentAmount.getText().toString().isEmpty()) {
+            int amount = Integer.parseInt(paymentAmount.getText().toString());
             filterCriteria.setPaymentAmount(amount);
         }
 
-        if(!bedroom.getText().toString().isEmpty()) {
-            int bedroomNum = Integer.parseInt( bedroom.getText().toString() );
+        if (!filter_no_of_bedrooms.getText().toString().isEmpty()) {
+            int bedroomNum = Integer.parseInt(filter_no_of_bedrooms.getText().toString());
             filterCriteria.setNumberOfBedrooms(bedroomNum);
         }
 
-        if(!bathroom.getText().toString().isEmpty()) {
-            int bathroomNum = Integer.parseInt( bathroom.getText().toString() );
+        if (!filter_no_of_bathrooms.getText().toString().isEmpty()) {
+            int bathroomNum = Integer.parseInt(filter_no_of_bathrooms.getText().toString());
             filterCriteria.setNumberOfBathrooms(bathroomNum);
         }
 
-        if(gasAvailability.isChecked()) {
+        if (gasAvailability.isChecked()) {
             filterCriteria.setGasAvailability(true);
         }
     }
 
     private Boolean noneSelected() {
         Boolean noneSelect = true;
-        EditText[] editTexts = {paymentAmount, bedroom, bathroom};
-        for(EditText t: editTexts) {
-            if(!t.getText().toString().isEmpty()) {
+        EditText[] editTexts = {paymentAmount, filter_no_of_bedrooms, filter_no_of_bathrooms};
+        for (EditText t : editTexts) {
+            if (!t.getText().toString().isEmpty()) {
                 Log.i(TAG, "edit tex selected");
                 noneSelect = false;
             }
         }
         int checkId = adType.getCheckedRadioButtonId();
-        if( checkId == rent.getId() || checkId == sale.getId()) {
+        if (checkId == rent.getId() || checkId == sale.getId()) {
             Log.i(TAG, "radio selected");
             noneSelect = false;
         }
-        if(gasAvailability.isChecked()) {
+        if (gasAvailability.isChecked()) {
             Log.i(TAG, "gas selected");
             noneSelect = false;
         }
